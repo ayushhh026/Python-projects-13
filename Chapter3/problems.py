9d2704e027ac1f3b65105fe2a45f156de3746c24
ayushhh26 <ayushshetty26@gmail.com>
2026-04-16 15:51:07 +0530

    strings

---
+++ Chapter3/problems.py
@@ -0,0 +1,9 @@
+
+
+Letter =   '''  Dear < | Name!!>,
+                You are selected!
+                <|Date|>  '''
+print(Letter.replace("< | Name!!>","AYUSH").replace("<|Date|>","Today").upper())
+
+name="Ayush is\n a\t \"good\" boy "
+print(name)
---
+++ Chapter3/strings.py
@@ -0,0 +1,11 @@
+name="  Ayush is OP  "
+print(len(name))
+print(name.startswith("Ay"))
+print(name.endswith("op"))
+print(name.lower())
+print(name.upper())
+print(name.count("h"))
+print(name.find("is"))
+print(name.strip())
+print(name.replace("OP","GOD"))
+
