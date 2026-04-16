7d0961bece10fc12ad1fc671bdb4b8cccfd97f9b
ayushhh26 <ayushshetty26@gmail.com>
2026-04-16 15:51:25 +0530

    data based

---
+++ Chapter4/lists.py
@@ -0,0 +1,14 @@
+marks =[]
+f1=int(input("Enter Marks name: "))
+marks.append(f1)
+f2=int(input("Enter Marks name: "))
+marks.append(f2)
+f3=int(input("Enter Marks name: "))
+marks.append(f3)
+f4=int(input("Enter Marks name: "))
+marks.append(f4)
+f5=int(input("Enter Marks name: "))
+marks.append(f5)
+marks.sort()
+
+print(sum(marks))
---
+++ Chapter4/tupple.py
@@ -0,0 +1,2 @@
+a=(1,0,22,4,45,0,"ayush","Harry")
+print(a.count(0))
