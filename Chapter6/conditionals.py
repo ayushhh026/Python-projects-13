7de8a033381a89ac5baffff3fb499e3249d496e1
ayushhh26 <ayushshetty26@gmail.com>
2026-04-17 16:30:20 +0530

    conditions

---
+++ Chapter6/conditionals.py
@@ -0,0 +1,15 @@
+a = int(input("Enter your age :"))
+if(a%2==0):
+    print("even no")
+
+if(a>18):
+    print("you are god")
+    print("you are legal")
+elif(a<0):
+    print("you are noob")
+elif(a==0):
+    print("abhi paida hua kya?")
+else:
+    print("you can not exist")
+
+print("End of code")
---
+++ Chapter6/problems.py
@@ -0,0 +1,19 @@
+# a = int(input("Enter marks of a subject :"))
+# b = int(input("Enter marks of a subject :"))
+# c = int(input("Enter marks of a subject :"))
+
+# total = ((a+b+c)/300)*100
+
+# if(total >= 40 and a>=33 and b>=33 and c>=33):
+    # print("you have passed",total)
+
+# else:
+    # print("you have failed try again",total)
+post=input("Enter the post : ")
+
+
+if("AYush".lower() in post.lower()):
+    print("This post is talking about ayush")
+
+else:
+    print("This post in not talking about ayush")
