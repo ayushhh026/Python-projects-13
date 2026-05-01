ded149caef378b7308b0d3e775294a66aff341f5
ayushhh26 <ayushshetty26@gmail.com>
2026-05-01 09:11:21 +0530

    snake-water-gun game

---
+++ Snake-water-gun/main.py
@@ -0,0 +1,35 @@
+
+import random
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+
+
+computer = random.choice([-1,1,0])
+youstr = input("Enter your choice: ")
+youDict = {"s":1, "w":-1, "g":0}
+reverseDict = {1:"snake", -1:"water", 0:"gun"}
+you = youDict[youstr]
+print(f"Computer chose {reverseDict[computer]}\n you chose {reverseDict[you]}")
+if(computer==you):
+    print("Its draw")
+else:
+    if(computer == -1 and you ==1):
+        print("You win")
+    elif(computer == -1 and you ==0):
+        print("You Lose")
+
+    elif(computer == 1 and you ==-1):
+        print("You lose")
+    elif(computer == 1 and you ==0):
+        print("You win")
+
+    elif(computer == 0 and you ==-1):
+        print("You win")
+    elif(computer == 0 and you ==1):
+        print("You lose")
+
+    else:
+        print("something went wrong")
